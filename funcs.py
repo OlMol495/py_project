3ba82a88557612bd8d21ce262f3a06085305dad2
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-09-15 23:27:56 +0800

    Added function deduct

--- funcs.py
+++ funcs.py
@@ -2,3 +2,8 @@ def add():
     a = int(input("Введите число"))
     b = int(input("Введите число"))
     return a + b
+
+def deduct():
+    a = int(input("Введите число"))
+    b = int(input("Введите число"))
+    return a - b
